b2c45fb6c38b74235345a3cd4f5664a3e99e0f86
hris25 <lokoharris25@gmail.com>
2025-07-08 11:16:50 +0100

    fixing 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,24 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
+  globalThis.__prisma ??
   new PrismaClient({
-    log: ['query'],
-  })
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+  globalThis.__prisma = prisma;
+}
